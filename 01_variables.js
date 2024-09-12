8a8e9d813b34bd658425d39fcd984c1fcd98f55e
Ubaid <juicyubaid@gmail.com>
2024-09-12 11:27:02 +0530

    intial code

---
+++ 01_variables.js
@@ -0,0 +1,22 @@
+const accountId = 12345678922
+let accountEmail ="ubaid@gmail.com"
+var accountPassword = "12345"
+accountcity = "Kashmir"
+
+
+
+// accountId = 2 // error // not allowed
+accountcity ="srinagar"
+accountEmail = "zaid@gmail.com"
+accountPassword="98869"
+
+// dont use acountId = "kashmir" it is a bad practice 
+// it possible to find this of code but not recommended
+console.log(accountId);
+  
+console.table([accountId, accountEmail, accountPassword, accountcity]);
+
+/*
+Prefer not to use  var
+because of issue with block scope and functional scope
+*/
