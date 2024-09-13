5fd261d2332772a2c600ae317ea40619ee7d3990
Ubaid <juicyubaid@gmail.com>
2024-09-13 11:50:39 +0530

    07-nums-and-maths

---
+++ 01-basics/07-nums-and-maths.js
@@ -0,0 +1,38 @@
+const score = 400
+// console.log(score);
+
+const balance = new Number(100)
+// console.log(balance);
+// console.log(balance.toFixed(4));
+// console.log(balance.toString());
+
+//  console.log(typeof balance);
+
+
+const otherNumber = 123.8966
+//console.log(otherNumber.toPrecision(4));
+
+const anotherNumber = 10000000
+//console.log(anotherNumber.toLocaleString('en-IN')); //puts commas
+
+
+//++++++++++++++++++++++MATHS+++++++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.PI);
+// console.log(Math.abs(-39));
+// console.log(Math.round(3.49));
+// console.log(Math.ceil(3.0001));  //rounds to next number even with a small decimal
+// console.log(Math.floor(4.99)); //rounds to previous number 
+// console.log(Math.max(1, 2, 3, 4, 5, 6, 7, 8,99, 10));
+// console.log(Math.min(9, 3, 5, 6, 7, 8, 1, 0.2, 4));
+console.log(Math.random() +1);
+console.log((Math.random() *10) +1);
+console.log(Math.floor(Math.random() *10) +1);
+
+const min = 10
+const max = 10
+console.log( Math.floor(Math.random() * ( max-min +1 )) + min);
+
+
+ 
