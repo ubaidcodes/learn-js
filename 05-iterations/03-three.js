289847524c707c5b75419771972ca4529b30c9a5
Ubaid <juicyubaid@gmail.com>
2024-09-21 15:16:00 +0530

    .\03-three.js

--- 05-iterations/03-three.js
+++ 05-iterations/03-three.js
@@ -1,21 +1,46 @@
 //for of 
 
-["","", ""] // array k andar strings
-[{},{},{}] // array k andar objects
+// ["","", ""] // array k andar strings
+// [{},{},{}] // array k andar objects
 
-const  arr = [1, 2, 3, 4, 5, 6]
+// const  arr = [1, 2, 3, 4, 5, 6]
 
-for (const element of arr) {
-    console.log(element);
+// for (const element of arr) {
+//     console.log(element);
+    
+// }
+
+    
+
+// const ggreetings = "hello world";
+// for (const greet of ggreetings) {
+//     if (greet === ' ') {  // Check if the character is a space
+//         continue;  // Skip to the next iteration if it is a space
+//     }
+//     console.log(`each character is ${greet}`);  // Print the character if it's not a space
+// }
+
+//MAPS 
+//uniques  hoti hai // doesnt print duplicate values
+
+const map = new Map()
+map.set ('IN ', "India")
+map.set ('USA', "United States of America")
+map.set ('FR ', "FRANCE")
+// console.log(map);
+
+for (const [key, value] of map) {
+    // console.log(key,":--" ,value);
     
 }
-
+// const myObj ={
+//     'game1' :"NFS",
+//     'game2' :"cyberpunk",
+//     'game3' :"RDR II"
+// }
+// for (const [key , value] of myObj) {
+//     console.log(key , ':__' , value);
     
+    
+// } // objects  iterable nahi hai
 
-const ggreetings = "hello world";
-for (const greet of ggreetings) {
-    if (greet === ' ') {  // Check if the character is a space
-        continue;  // Skip to the next iteration if it is a space
-    }
-    console.log(`each character is ${greet}`);  // Print the character if it's not a space
-}
