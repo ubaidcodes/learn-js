7262145fcb05035413bf94f134c97b95efca2ed5
Ubaid <juicyubaid@gmail.com>
2024-09-21 15:52:41 +0530

    for in loop

---
+++ 05-iterations/04-four.js
@@ -0,0 +1,32 @@
+//for in loop
+// maps mai forin loop nahi itterate hota hai aur forof loop objects pe itterate nahi hota hai
+const myObject = {
+    js : 'javascript ',
+    cpp: 'c++',
+    rb: 'ruby',
+    flutter:'GOOGLE'
+
+} // yaha pe forin loop use hota for objects
+
+for (const key in myObject) {
+    // console.log(key);
+    // console.log(myObject[key]);
+    // console.log(`${key} is shortcut for ${myObject[key]}`);        
+}
+
+//for array
+
+const programming = ['js', 'cpp', 'rb', 'flutter', 'java']
+
+for (const key in programming) {
+    // console.log(key);
+    // console.log(programming[key]);
+    
+
+    
+    
+}
+
+
+
+
