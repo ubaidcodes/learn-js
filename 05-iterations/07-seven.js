171714332fb3ed718305dc97f95517c20808833d
Ubaid <juicyubaid@gmail.com>
2024-09-22 22:12:34 +0530

    seven

--- 05-iterations/07-seven.js
+++ 05-iterations/07-seven.js
@@ -7,7 +7,7 @@
  //chaining
  const myNums =[1, 2,3,4,5,6,7,8,9,10 ]
  const newNums = myNums
-                .map((num) =>num *10)
+                .map((num) =>num *12)
                 .map((num) => num+2)
                 .filter( (num) => num <67)
 
